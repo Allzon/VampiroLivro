196b8a0158b41c67244bc044bb2aa2e9cc83476d
Lucas-64 <77701545+Lucas-64@users.noreply.github.com>
2022-06-22 23:23:58 -0300

    imports fix

---
+++ src/theme/MDXComponents/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import {Titulo} from '@site/src/components/title'
+import {Paragrafo} from '@site/src/components/title'
+import Quadro from '@site/src/components/Quadro'
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  // Map the "highlight" tag to our <Highlight /> component!
+  // `Highlight` will receive all props that were passed to `highlight` in MDX
+  Titulo: Titulo,
+  Paragrafo: Paragrafo,
+  Quadro: Quadro
+};
