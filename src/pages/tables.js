067c058e15f55055e940172340a35dff079339de
Guilherme de Jesus <Guilherme.ichiyon@gmail.com>
2022-06-24 13:09:14 -0300

    test

--- src/pages/tables.js
+++ src/pages/tables.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import TableClans from "/src/components/tables/_clans.mdx";
+import TableClans from "../components/Tables/_clans.mdx";
 
 export default function Hello() {
   return (
