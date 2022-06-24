bfdd1f433b8e821f6e9ac4621b17cfa3b8da0805
Guilherme de Jesus <Guilherme.ichiyon@gmail.com>
2022-06-24 13:20:07 -0300

    desisto

--- src/pages/tables.js
+++ src/pages/tables.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import TableClans from "../components/Tables/_clans.mdx";
+import TableClans from "@site/src/components/Tables/_clans.mdx";
 
 export default function Hello() {
   return (
