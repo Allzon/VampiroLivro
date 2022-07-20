f28eb3b6a92c4c5afcc0cf9265f770683591de9a
Guilherme de Jesus <Guilherme.braga@fitbank.com.br>
2022-07-20 12:54:34 -0300

    Remove button switch colortheme and config updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
     ({
       colorMode: {
         defaultMode: "dark",
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       navbar: {
